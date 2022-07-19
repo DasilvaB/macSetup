eced2f121eddbbd3895c635dc4673d5d68f72c82
DasilvaB <bdbrunodasilva@hotmail.com>
2022-07-19 17:18:53 -0400

    adding treesitter colors

--- nvim/lua/user/treesitter.lua
+++ nvim/lua/user/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "java" },
+  ensure_installed = { "java", "lua", "go" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
