99f0e33455304f84e6508dba14a69557540e7bb5
Wooshj <bdbrunodasilva@Hotmail.com>
2022-12-07 19:55:29 -0500

    changes (#7)
    Co-authored-by: Bruno Dasilva <bdasilva@confluent.io>

--- nvim/lua/user/colorscheme.lua
+++ nvim/lua/user/colorscheme.lua
@@ -1,11 +1,6 @@
 vim.cmd [[
   try
-    if strftime("%H") >= 8 && strftime("%H") <= 19
-      set background=light
-      colorscheme catppuccin
-    else
-      colorscheme catppuccin 
-    endif
+    colorscheme gruvbox
   catch /^Vim\%((\a\+)\)\=E185/
     colorscheme default
     set background=dark
--- nvim/lua/user/treesitter.lua
+++ nvim/lua/user/treesitter.lua
@@ -1,6 +1,7 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "go", "java", "lua", "kotlin", "rust", "python" },
+
+  ensure_installed = { "hcl", "go", "java", "lua", "kotlin", "rust", "python" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
